88c68994b6eff4e4fadc85651d5b70e8a1424cc0
Hoang Huan <huan12345ndnd@gmail.com>
2024-12-29 12:22:53 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,17 @@
-// ()=>{}
+// JSX : 1 parent
+// Fragment
+
+import './style.css';
 
 const MyComponent = () => {
     return (
-        <div>Hoàng Huân nè update</div>
+        <>
+            <div >Hoàng Huân nè update</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
+
     );
 
 }
